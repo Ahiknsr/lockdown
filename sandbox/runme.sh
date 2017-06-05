616d34c4bd18cdce348eeec7831adf4b7d171e4a
Ahiknsr <gorantla.sai5@gmail.com>
2017-06-05 07:38:15 +0530

    updated the playground endpoint to allow c and c++ code execution in sandbox

--- sandbox/runme.sh
+++ sandbox/runme.sh
@@ -18,17 +18,24 @@ else
     exit
 fi
 #echo $compiler
-if [ ! -f test ]; 
-then
-    gcc test.c -o test
-fi
+main_func()
+{
 
-if [ ! -f execute ]; 
-then
-    gcc execute.c -o execute
-fi
+    cd "${0%/*}"
+    if [ ! -f test ]; 
+    then
+        gcc test.c -o test
+    fi
 
-#need to compile using static flag since we block the process from opening new files
-$compiler $1 -o source -static
-./execute source < $2 > outfile
-rm source
+    if [ ! -f execute ]; 
+    then
+        gcc execute.c -o execute
+    fi
+
+    #need to compile using static flag since we block the process from opening new files
+    export PATH=$PATH:.
+    $compiler $1 -o source_exe -static
+    ./execute source_exe < $2 > outfile
+    rm source_exe
+}
+main_func "$@"
--- server/lockdown/views.py
+++ server/lockdown/views.py
@@ -3,13 +3,19 @@ from django.http import HttpResponse
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 
-import json
+import json, tempfile, shutil
+from os import path, system
 
 from .forms import LoginForm
 
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the home page.")
+    if request.user.is_authenticated:
+        return redirect('playground')
+    else:
+        return redirect('signup')
 
 
 def signup(request):
@@ -32,7 +38,7 @@ def user_login(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponse("valid login")
+            return redirect('playground')
         else:
             return HttpResponse("invalid login")
     else:
@@ -58,7 +64,33 @@ def runcode(request):
     if (not request.user.is_authenticated) or request.method == 'GET':
         return HttpResponse('Unauthorized', status=401)
     else:
-        print request.POST.viewvalues()
         received_data = json.loads(request.body)
-        print received_data['code']
-        return HttpResponse('okay')
+        maketempdir(request.user.username)
+        createfile('source', received_data['code'], request.user.username, received_data['lang'])
+        createfile('input', received_data['input'], request.user.username)
+        tmp_user_dir = '/tmp/' + request.user.username + '/sandbox/'
+        source_file = tmp_user_dir + 'source.' + received_data['lang']
+        input_file = tmp_user_dir + 'input.txt'
+        command = tmp_user_dir + './runme.sh ' + source_file + ' ' + input_file
+        system(command)
+        return HttpResponse(readoutput(request.user.username))
+
+
+def maketempdir(username):
+    main_dir = path.join(path.dirname(settings.BASE_DIR),settings.SANDBOX_DIR)
+    tmp_user_dir = '/tmp/' + username + '/sandbox'
+    if not path.exists(tmp_user_dir):
+        shutil.copytree(main_dir,tmp_user_dir)
+
+def createfile(fname, content, username, ext='txt'):
+    tmp_user_dir = '/tmp/' + username + '/sandbox/'
+    f = open(tmp_user_dir + fname + '.' + ext , 'w')
+    f.write(content)
+    f.close()
+
+def readoutput(username):
+    outfile = '/tmp/' + username + '/sandbox/' + 'outfile'
+    with  open(outfile,'r') as f:
+        return f.read()
+    
+    
